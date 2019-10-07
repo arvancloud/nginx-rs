cb3518ac33a88d7fcaf9f5fc19b31db7b66d649f
Navid <yaa110@gmail.com>
2019-10-07 15:14:42 +0330

    Support NGINX_PATH environment variable to build pre-cloned nginx

--- build.rs
+++ build.rs
@@ -1,12 +1,13 @@
 extern crate bindgen;
 
 use std::env;
+use std::io::Result;
 use std::path::{Path, PathBuf};
 use std::process::Command;
 
-const NGINX_VERSION: &'static str = "1.15.8";
+const NGINX_VERSION: &'static str = "1.17.4";
 
-fn run_make(rule: &str, cwd: &Path) -> Option<bool> {
+fn run_make(rule: &str, cwd: &Path, local_nginx_path: &str) -> Result<bool> {
     let output = Command::new("make")
         .arg(rule)
         .env("OUT_DIR", env::var("OUT_DIR").unwrap())
@@ -14,24 +15,48 @@ fn run_make(rule: &str, cwd: &Path) -> Option<bool> {
             "NGINX_VERSION",
             env::var("NGINX_VERSION").unwrap_or(NGINX_VERSION.to_string()),
         )
+        .env("NGINX_PATH", local_nginx_path)
         .current_dir(cwd)
-        .output()
-        .ok()?;
-    Some(output.status.success())
+        .output()?;
+    Ok(output.status.success())
 }
 
 fn main() {
     println!("cargo:rerun-if-env-changed=NGINX_VERSION");
+    println!("cargo:rerun-if-env-changed=NGINX_PATH");
 
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
 
     let cwd = env::current_dir().unwrap();
-    run_make("prepare-nginx", cwd.as_path())
-        .and_then(|success| if success { Some(()) } else { None })
-        .expect("unable to execute `make prepare-nginx`");
+    let local_nginx_path = env::var("NGINX_PATH").unwrap_or_default();
+
+    run_make(
+        if local_nginx_path.is_empty() {
+            "prepare-nginx"
+        } else {
+            "prepare-nginx-local"
+        },
+        cwd.as_path(),
+        local_nginx_path.as_str(),
+    )
+    .map_err(|e| e.to_string())
+    .and_then(|success| {
+        if success {
+            Ok(())
+        } else {
+            Err(String::from(
+                "preparing nginx exited with non-zero status code",
+            ))
+        }
+    })
+    .expect("unable to prepare nginx");
 
     let nginx_dir_path = out_path.join("nginx");
-    let nginx_dir = nginx_dir_path.to_str().unwrap();
+    let nginx_dir = if local_nginx_path.is_empty() {
+        nginx_dir_path.to_str().unwrap()
+    } else {
+        local_nginx_path.as_str()
+    };
 
     let bindings = bindgen::Builder::default()
         .header("wrapper.h")
@@ -43,6 +68,7 @@ fn main() {
             format!("-I{}/src/os/unix", nginx_dir),
             format!("-I{}/objs", nginx_dir),
             format!("-I{}/src/http", nginx_dir),
+            format!("-I{}/src/http/v2", nginx_dir),
             format!("-I{}/src/http/modules", nginx_dir),
         ])
         .generate()
