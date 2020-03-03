27190fa20272000f2f904c2400ae34210f58849e
Navid <yaa110@gmail.com>
2020-03-04 00:26:25 +0330

    Update documentation

---
+++ implementors/core/default/trait.Default.js
@@ -0,0 +1,9 @@
+(function() {var implementors = {};
+implementors["nginx"] = [{"text":"impl&lt;Storage:&nbsp;<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a>, Align:&nbsp;<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"nginx/struct.__BindgenBitfieldUnit.html\" title=\"struct nginx::__BindgenBitfieldUnit\">__BindgenBitfieldUnit</a>&lt;Storage, Align&gt;","synthetic":false,"types":["nginx::bindings::__BindgenBitfieldUnit"]},{"text":"impl&lt;T:&nbsp;<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"nginx/struct.__IncompleteArrayField.html\" title=\"struct nginx::__IncompleteArrayField\">__IncompleteArrayField</a>&lt;T&gt;","synthetic":false,"types":["nginx::bindings::__IncompleteArrayField"]},{"text":"impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"nginx/struct.__BindgenUnionField.html\" title=\"struct nginx::__BindgenUnionField\">__BindgenUnionField</a>&lt;T&gt;","synthetic":false,"types":["nginx::bindings::__BindgenUnionField"]}];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
---
+++ source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["nginx"] = {"name":"","files":["bindings.rs","helpers.rs","lib.rs","log.rs"]};
+createSourceSidebar();
